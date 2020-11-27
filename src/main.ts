b6b6e29b19e90c022452b70748d4798e109cf6c8
clayton-duarte <clayton@null.net>
2020-11-27 16:41:14 -0500

    :sparkles: Set all non-owners as reviewers

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,35 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 
 import { WebhookPayload } from '@actions/github/lib/interfaces'
-// import { GitHub } from '@actions/github/lib/utils'
+import { GitHub } from '@actions/github/lib/utils'
 
-import { formatMessage } from './utils'
+// import { formatMessage } from './utils'
 
 async function run(): Promise<void> {
   try {
     // SETUP
     const github_token: string = core.getInput('github_token')
-    core.debug(formatMessage(github_token != null, 'github_token exists'))
+    const possible_reviewers: string = core.getInput('possible_reviewers')
+    const octokit: InstanceType<typeof GitHub> = github.getOctokit(github_token)
 
     const pushPayload: WebhookPayload = github.context.payload
-    const pullId: string = pushPayload.pull_request?.id
-    core.debug(formatMessage(pullId, 'pullId'))
-    const owner: string = pushPayload.sender?.login || ''
-    core.debug(formatMessage(owner, 'owner'))
+    const pull_number: number = pushPayload.pull_request?.number || 0
     const repo: string = pushPayload.repository?.name || ''
-    core.debug(formatMessage(repo, 'repo'))
+    const owner: string = pushPayload.sender?.login || ''
 
-    const octokit = github.getOctokit(github_token)
-    const pullRequests = await octokit.pulls.list({
+    // ACTION
+    const reviewers_list = possible_reviewers.split(',')
+    const reviewers = reviewers_list.filter((login) => login !== owner)
+    //octokit.github.io/rest.js/v18#pulls-request-reviewers
+
+    const result = await octokit.pulls.requestReviewers({
+      pull_number,
+      reviewers,
       owner,
       repo
     })
-    core.debug(formatMessage(pullRequests, 'pullRequests'))
 
-    core.setOutput('owner', owner)
+    core.setOutput('result', result)
   } catch (error) {
     core.setFailed(error)
   }
